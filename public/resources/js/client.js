40638682df581d2661d5afe2cba3a96dfe219213
neodev2 <undefined>
2017-04-20 03:44:10 +0200

    edits

--- public/resources/js/client.js
+++ public/resources/js/client.js
@@ -1,15 +1,11 @@
 var socket = io();
-
+var data;
 
 setInterval(function(){
-	
 	socket.emit('update', true);
-	
 }, 4000);
 
-
 socket.on('update', function(data){
-	
 	console.log(data);
-	
+	data = data;
 });
