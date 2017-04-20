32e84a86b34a7015bc90a368cac603ea06723237
neodev2 <undefined>
2017-04-20 05:06:44 +0200

    edits

--- public/resources/js/client.js
+++ public/resources/js/client.js
@@ -7,22 +7,22 @@ var socket = io();
 socket.on('update', function(data){
 	//console.log(data);
 	
-	var html = `<div class="channel">`;
+	var html = '';
 	
 	for(let channel_name in data){
-		html += `<div><div>${channel_name}</div>`;
+		html += `<div data-name="${channel_name}">`;
 		
 		for(let msgId in data[channel_name]){	
 			html += 
 			`<div id="${msgId}">
-				<div>${data[channel_name][msgId]['m_time']}</div>
-				<div>${data[channel_name][msgId]['m_content']}</div>
-				<div>${data[channel_name][msgId]['m_authorUsername']}</div>
-			<div></div>`;	
+				<i>${data[channel_name][msgId]['m_time']}</i>
+				<p>${data[channel_name][msgId]['m_content']}</p>
+				<small>${data[channel_name][msgId]['m_authorUsername']}</small>
+			<div>`;	
 		}
+		
+		html += '</div>';
 	}
-	
-	html += `</div>`;
 	
 	$('body').html(html);
 	
