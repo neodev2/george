564833205bdcd78d674a43f9112d39bec714a4b5
neodev2 <undefined>
2017-04-20 03:27:33 +0200

    edits

--- public/resources/js/client.js
+++ public/resources/js/client.js
@@ -12,12 +12,24 @@ socket.on('update', function(data){
 	
 	console.log(data);
 	
-	for(let channel in data){
-		for(let msgId in channel){
-			console.log(data[msgId].m_authorUsername);
-			console.log(data[msgId].m_content);
-			console.log(data[msgId].m_time);
+	
+	var html = '';
+	
+	for(let channelName in data){
+		
+		html += '<div id="'+channelName+'">';
+		
+		for(let msgId in channelName){
+			html += '<div>'+data[msgId].m_authorUsername+'</div>';
+			html += '<div>'+data[msgId].m_content+'</div>';
+			html += '<div>'+data[msgId].m_time+'</div>';
 		}
+		
+		html += '</div>';
+		
+		
+		$('body').html(html)
+		
 	}
 	
 });
