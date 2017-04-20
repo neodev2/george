975ab3ec45eb042221c43331a2dffedcd8d8202b
neodev2 <undefined>
2017-04-20 05:38:29 +0200

    edits

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ io.on('connection', function(socket){
 			
 			response[channel.name] = {};
 			
-			channel.fetchMessages({limit: 10})
+			channel.fetchMessages({limit: 3})
 			.then(function(messages){
 				//console.log(`Found ${messages.size} messages`);
 				
