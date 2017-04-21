55153035217e18cd9999a74e2ad83492fbd96d99
neodev2 <undefined>
2017-04-21 07:19:15 +0200

    edits

---
+++ README.md
@@ -0,0 +1,26 @@
+# george
+
+a discord bot
+<br>
+<br>
+
+<h3>Some example commands:</h3>
+<br>
+
+<b id="wiki">`/wiki <name>#<section>`</b>: searches on wikipedia, example:
+
+    /wiki something
+
+    /wiki something#Philosophy_and_language
+
+    /wiki Something_(concept)
+
+<b id="george">`george`</b>: answers "?"
+
+<b id="ping">`ping`</b>: answers "pong"
+
+<b id="clear">`clear`</b>: deletes last 100 messages of the current channel
+
+<br>
+
+[stats DEMO](https://george123.herokuapp.com/)
---
+++ public/resources/js/client.js
@@ -0,0 +1,29 @@
+var socket = io();
+
+setInterval(function(){
+	socket.emit('update', true);
+}, 314);
+
+socket.on('update', function(data){
+	//console.log(data);
+	
+	var html = '';
+	
+	for(let channel_name in data){
+		html += `<div data-name="${channel_name}"><h4>${channel_name}</h4>`;
+		
+		for(let msgId in data[channel_name]){	
+			html += 
+			`<div id="${msgId}">
+				<i>${data[channel_name][msgId]['m_time']}</i>
+				<p>${data[channel_name][msgId]['m_content']}</p>
+				<small>${data[channel_name][msgId]['m_authorUsername']}</small>
+			</div>`;	
+		}
+		
+		html += '</div>';
+	}
+	
+	$('body').html(html);
+	
+});
